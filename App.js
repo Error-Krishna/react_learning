9c0462112c41772557112206920312717be4cfe4
Krishna Goyal <iamkrishnagoyal@gmail.com>
2026-07-20 12:20:08 +0530

    Update App.js to create multiple child elements and reorganize index.html script tags; add lecture notes on React fundamentals

--- App.js
+++ App.js
@@ -8,17 +8,21 @@
 
 
 
-const parent = React.createElement(
-    "div", 
-    {id:"parent"}, 
-        React.createElement(
-            "div", {id:"child"},
+const parent = React.createElement("div", {id:"parent"}, [
+        React.createElement("div", {id:"child"},
+            [
+                React.createElement("h1", {}, "I am an <h1></h1> Tag"), 
+                React.createElement("h2", {}, "I am an <h2></h2> Tag")
+            ]
+        ),
+        React.createElement("div", {id:"child"},
             [
                 React.createElement("h1", {}, "I am an <h1></h1> Tag"), 
                 React.createElement("h2", {}, "I am an <h2></h2> Tag")
             ]
         )
-    );
+    ]
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
